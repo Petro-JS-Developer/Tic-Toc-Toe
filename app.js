7f6a87ecfd73ece0c6b9109c4d7ae57ec2cb920f
Petro Mandzyuk <petrom333@gmail.com>
2022-07-28 21:07:46 +0300

    add disabled button and cell on win

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let XValuePlayer = 'X';
-let OValuePlayer = 'O';
+const XValuePlayer = "X";
+let OValuePlayer = "O";
 let player = "";
 
 let buttonCrossElement = document.getElementById(XValuePlayer);
@@ -7,12 +7,15 @@ let buttonZeroElement = document.getElementById(OValuePlayer);
 
 buttonCrossElement.addEventListener("click", () => {
   player = buttonCrossElement.innerText;
-})
+  document.getElementById("X").classList.add("disabled");
+  document.getElementById("O").classList.add("disabled");
+});
 
 buttonZeroElement.addEventListener("click", () => {
   player = buttonZeroElement.innerText;
-})
-
+  document.getElementById("X").classList.add("disabled");
+  document.getElementById("O").classList.add("disabled");
+});
 
 const arrayWinCombinations = [
   [0, 1, 2],
@@ -25,238 +28,145 @@ const arrayWinCombinations = [
   [2, 4, 6],
 ];
 
+const playingField = document.getElementById("playingField");
+
+let arrCurrentValuesOnPlayingFields = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
 const convertIndexInNumber = (row, column) => {
   const key = row + column;
+
   switch (key) {
-    case 0: return 0;
-    case 1: return 1;
-    case 2: return 2;
-    case 2: return 2;
-
-      break;
-
-    default:
-      break;
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    case 2:
+      return 2;
   }
-}
+};
 
 const convertingValueInArrIndexX_O = (allCell) => {
-  // const arrXCombination = [
-  //   ["", "", ""],
-  //   ["", "", ""],
-  //   ["", "", ""],
-  // ];
   const arrXCombination = [];
-  const arrOCombination = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+
+  const arrOCombination = [];
 
   allCell.forEach((arrValues, index) => {
     arrValues.forEach((value, i) => {
       if (value === XValuePlayer) {
         if (index === 0) {
-          // arrXCombination[index][i] = index + i;
-          arrXCombination.push(index + i)
+          arrXCombination.push(index + i);
         }
         if (index === 1) {
-          // arrXCombination[index][i] = 3 + i;
-          arrXCombination.push(3 + i)
+          arrXCombination.push(3 + i);
         }
         if (index === 2) {
-          // arrXCombination[index][i] = 6 + i;
-          arrXCombination.push(6 + i)
+          arrXCombination.push(6 + i);
         }
       }
 
       if (value === OValuePlayer) {
         if (index === 0) {
-          arrOCombination[index][i] = index + i;
+          arrOCombination.push(index + i);
         }
         if (index === 1) {
-          arrOCombination[index][i] = 3 + i;
+          arrOCombination.push(3 + i);
         }
         if (index === 2) {
-          arrOCombination[index][i] = 6 + i;
+          arrOCombination.push(6 + i);
         }
       }
-    })
-  })
+    });
+  });
 
   return {
     X_arr_indexes: arrXCombination,
     O_arr_indexes: arrOCombination,
-  }
-}
+  };
+};
 
 const getArrPotentialWinCombination = (valueIndex) => {
-  return arrayWinCombinations.filter((arrCombination) => arrCombination[0] === valueIndex)
-}
-
-const checkIfThereIsWinner = (arrAllCell) => {
-
-
-  const { X_arr_indexes, O_arr_indexes } = convertingValueInArrIndexX_O(arrAllCell)
-
-  const findThreeTrue = (arrWinCombinationForCurrentValue, arrAllValues) => {
-    for (let i = 0; i < arrWinCombinationForCurrentValue.length; i++) {
-      const nestedArrayWinIndexes = arrWinCombinationForCurrentValue[i];
-
-      const win = nestedArrayWinIndexes.every((winNumber) => arrAllValues.includes(winNumber))
-      if (win) {
-        return true;
-      }
-
-    }
-    return false;
-  }
-
-  const checkWin = (arrIndexPlayer, player) => {
-    for (let i = 0; i < arrIndexPlayer.length; i++) {
-      const nestedArrayIndexes = arrIndexPlayer[i];
-      const arrPotentialWinCombination = getArrPotentialWinCombination(nestedArrayIndexes);
-
-      if (findThreeTrue(arrPotentialWinCombination, X_arr_indexes)) {
-        return player;
-      }
-    }
-  }
-
-  if (checkWin(X_arr_indexes, "X")) {
-    alert(`Winner player ${checkWin(X_arr_indexes, "X")}`)
-  }
-  console.log({ getWin: checkWin(X_arr_indexes, "X") })
-
-
-  // TODO: беремо масив з записаними числами перебираємо знаходимо число берем його і його індекс в масиві
-  // TODO: перебираємо масив виграшних комбінацій порінюємо підмасиви щоб співпадали індекси і значння в під масиві фільтруємо 
-
-
-  // const winX = () => {
-
-  //   let filteredWInArr = [];
-  //   arrXCombination.some((arrValues, index) => {
-  //     arrValues.some((numberValues, i) => {
-  //       if (!numberValues) {
-  //         return false
-  //       }
-
-  //       filteredWInArr = arrayWinCombinations.filter((rowCombinationArr) => {
-  //         // debugger;
-
-  //         console.log({ booleun: rowCombinationArr[i] === numberValues, rowCombinationArr: rowCombinationArr[i], numberValues })
-  //         return rowCombinationArr[i] === numberValues
-  //       })
-  //     })
-  //   })
-  //   console.log({ filteredWInArr })
-  //   // arrXCombination.some((arrValues, index) => {
-  //   //   arrValues.some((numberValues, i) => {
-  //   //     if (!numberValues) {
-  //   //       return false
-  //   //     }
-  //   //     const filteredWInArr = arrayWinCombinations.filter((rowCombinationArr) => rowCombinationArr[i] === numberValues)
-
-  //   //   })
-  //   // })
-  //   // arrayWinCombinations.filter((rowCombinationArr, index) => {
-  //   //   debugger;
-
-  //   //   return rowCombinationArr.some((currentNumber, i) => {
-  //   //     if (i === 0) {
-  //   //       return arrXCombination[index][i] === currentNumber || arrOCombination[index][i] === currentNumber
-  //   //     } else {
-  //   //       return false
-  //   //     }
-  //   //   })
-
-  //   // });
-
-  // }
-  // winX()
-  // const filteredArrWithWinCombination = arrayWinCombinations.filter((rowCombinationArr, index) => {
-
-  //   return rowCombinationArr.some((currentNumber, i) => {
-  //     if (i === 0) {
-  //       return arrXCombination[index][i] === currentNumber || arrOCombination[index][i] === currentNumber
-  //     } else {
-  //       return false
-  //     }
-  //   })
-
-  // });
-
-  // const win = filteredArrWithWinCombination.some((rowCombinationArr, index) => {
-
-  //   let XWin = 0;
-  //   let OWin = 0;
-  //   rowCombinationArr.filter((currentNumber, i) => {
-  //     if (i === 0) {
-  //       return arrXCombination[index][i] === currentNumber || arrOCombination[index][i] === currentNumber
-  //     } else {
-  //       return false
-  //     }
-  //   }).forEach((currentNumber, i) => {
-  //     if (arrXCombination[index][i] === currentNumber) {
-  //       XWin++
-  //     }
-
-  //     if (arrOCombination[index][i] === currentNumber) {
-  //       OWin++
-  //     }
-  //   })
-  //   if (XWin === 3) {
-  //     return alert("Win player X")
-  //   }
-  //   if (OWin === 3) {
-  //     return alert("Win player O")
-  //   }
-  // });
-
-  // console.log({ win })
-
-}
-
-const playingField = document.getElementById("playingField")
-
-const arrCurrentValuesOnPlayingFields = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+  return arrayWinCombinations.filter(
+    (arrCombination) => arrCombination[0] === valueIndex
+  );
+};
 
 const getArrAllCells = (parentElement) => {
   let arrAllCell = [];
   for (let index = 0; index < parentElement.children.length; index++) {
     const children = [...parentElement.children[index].children];
     children.forEach((cell, i) => {
-      arrCurrentValuesOnPlayingFields[index][i] = cell.innerText
-      arrAllCell.push(cell)
-    })
+      arrCurrentValuesOnPlayingFields[index][i] = cell.innerText;
+      arrAllCell.push(cell);
+    });
   }
 
-  return arrAllCell
-}
+  return arrAllCell;
+};
 
-playingField.addEventListener('click', (event) => {
+const checkIfThereIsWinner = (arrAllCell) => {
+  const { X_arr_indexes, O_arr_indexes } =
+    convertingValueInArrIndexX_O(arrAllCell);
+
+  const findThreeTrue = (arrWinCombinationForCurrentValue, arrAllValues) => {
+    for (let i = 0; i < arrWinCombinationForCurrentValue.length; i++) {
+      const nestedArrayWinIndexes = arrWinCombinationForCurrentValue[i];
+
+      const win = nestedArrayWinIndexes.every((winNumber) =>
+        arrAllValues.includes(winNumber)
+      );
+      if (win) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const checkWin = (arrIndexPlayer, player) => {
+    for (let i = 0; i < arrIndexPlayer.length; i++) {
+      const nestedArrayIndexes = arrIndexPlayer[i];
+      const arrPotentialWinCombination =
+        getArrPotentialWinCombination(nestedArrayIndexes);
+
+      if (findThreeTrue(arrPotentialWinCombination, arrIndexPlayer)) {
+        return player;
+      }
+    }
+  };
+
+  if (checkWin(X_arr_indexes, "X")) {
+    alert(`Winner player ${checkWin(X_arr_indexes, "X")}`);
+    playingField.classList.add("disabled");
+  }
+
+  if (checkWin(O_arr_indexes, "O")) {
+    alert(`Winner player ${checkWin(O_arr_indexes, "O")}`);
+    playingField.classList.add("disabled");
+  }
+};
+
+playingField.addEventListener("click", (event) => {
   const currentCell = event.target;
 
   if (player === XValuePlayer) {
     currentCell.innerText = player;
-    player = OValuePlayer
+    player = OValuePlayer;
   } else if (player === OValuePlayer) {
     currentCell.innerText = player;
-    player = XValuePlayer
+    player = XValuePlayer;
   }
-  console.log({ getArrAllCells: getArrAllCells(playingField), arrCurrentValuesOnPlayingFields, check: checkIfThereIsWinner(arrCurrentValuesOnPlayingFields) })
 
-})
-
-
+  getArrAllCells(playingField);
+  checkIfThereIsWinner(arrCurrentValuesOnPlayingFields);
+});
 
 // TODO: Step one save value in arrValues
 // TODO: Step two check win Сombination
-// TODO: Step three display a win message
+// TODO: Step three display a win message and cross out the winning combination
+
 // TODO: Step four disable button after choosing who you play for
-// TODO: Step five cross out the winning combination
+
+// TODO: Step six if all cells are filled view message "Draw"
